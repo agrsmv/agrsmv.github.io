d51165f568ed6edaf47659504fd7cb6866adac24
agrsmv <andreyrgerasimov@gmail.com>
2022-11-21 21:39:55 +0300

    Fixed name function

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function info() {
     var x = document.getElementById("info");
-    if (x.innerHTML === "Andrei Gerasimov") {
+    if (x.innerHTML === "Andrei&nbspGerasimov") {
       x.innerHTML = "Andrei Gerasimov is a graphic designer. He graduated from Moscow’s HSE Art & Design School in 2022.";
     } else {
-      x.innerHTML = "Andrei Gerasimov";
+      x.innerHTML = "Andrei&nbspGerasimov";
     }
   }
