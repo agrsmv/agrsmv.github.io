8e77df182876a7770cf272de066c22330d71f402
agrsmv <andreyrgerasimov@gmail.com>
2022-11-22 19:47:50 +0300

    Flexbox responsive 6-columns with empty columns

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function info() {
-    var x = document.getElementById("info");
+    var x = document.getElementById("website-title");
     if (x.innerHTML === "Andrei Gerasimov") {
       x.innerHTML = "Andrei Gerasimov is a graphic designer. He graduated from Moscow’s HSE Art & Design School in 2022.";
+      x.style.whiteSpace = "normal";
     } else {
       x.innerHTML = "Andrei Gerasimov";
+      x.style.whiteSpace = "nowrap";
     }
   }
