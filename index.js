68f1c0c4751408e7bc32df6d22f1d8ddbcd99db9
agrsmv <andreyrgerasimov@gmail.com>
2023-01-08 02:29:35 +0300

    upd

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function info() {
     var x = document.getElementById("website-title");
     if (x.innerHTML === "Andrei Gerasimov") {
-      x.innerHTML = "Andrei Gerasimov is a graphic designer. He graduated from HSE Art and Design&nbspSchool.";
+      x.innerHTML = "Andrei Gerasimov is a graphic designer. He graduated from HSE Art and Design&nbspSchool (BA&nbsp’22).";
       x.style.whiteSpace = "normal";
     } else {
       x.innerHTML = "Andrei Gerasimov";
