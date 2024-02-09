b3076311622ae91c081c996583a87945f1916aea
agrsmv <andreyrgerasimov@gmail.com>
2024-02-09 16:42:47 +0100

    deleted archive

--- archive/v1/showhide.js
+++
@@ -1,8 +0,0 @@
-function showhide(el) {
-    var x = document.getElementById(el);
-    if (x.style.display === "none") {
-      x.style.display = "flex";
-    } else {
-      x.style.display = "none";
-    }
-  }
